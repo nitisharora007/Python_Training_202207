1d198ccc06adf957b356188b62c5014f28cfd818
Nitish Arora <nitisharora007@gmail.com>
2022-07-31 14:42:14 +0530

    Added class practice question - 1

---
+++ exercise/classes.py
@@ -0,0 +1,21 @@
+'''
+Write a python class named as Student, which will be having attributes as fisrtname, lastname, dateof birth
+method(s): getName -> which will concatenate firstname and lastname
+getAge -> which will use the date of birth or year of birth for age calculation   => make this private
+isEligible -> which will return True if the student age is less than 4
+
+'''
+
+class Student:
+
+    def __init__(self, firstname, lastname, dob) -> None:
+        pass
+
+    def __getAge(self):
+        pass
+
+    def isEligible(self):
+        pass
+
+
+
